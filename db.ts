dd30b7f11d275ac1912a415054f74ac1b6e406bf
pinkadotted <m.subesh@gmail.com>
2023-12-28 01:51:00 +0800

    created a connectDB fn to connect to db

---
+++ db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI || "", {
+      dbName: "AureusDB",
+    });
+
+    console.log(`Server connected to database ${connection.host}`);
+  } catch (error) {
+    console.log("Some error occurred. ", error);
+    process.exit(1);
+  }
+};
