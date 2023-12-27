768e6c52f7e6537733b83f1d5a95d0383c8378ee
pinkadotted <m.subesh@gmail.com>
2023-12-28 00:17:37 +0800

    created Users schema

---
+++ models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const schema = new mongoose.Schema({
+    email: { type: String, 
+             required: [true, "Please enter an email"], 
+             unique: [true, "This email already exists!"],
+             validate: validator.isEmail }, // validates email using the validator package
+    firstName: { type: String, 
+                required: [true, "Please enter a first name"] },
+    lastName: { type: String, 
+                required: [true, "Please enter a last name"] },
+    password: { type: String, 
+                required: [true, "Please enter a password"],
+                select: false }, // this will prevent the password from being returned in any query
+})
+
+
+schema.pre("save", async function() {
+    // hash the password before saving the user
+    this.password = await bcrypt.hash(this.password, 12);
+})
+
+
+schema.methods.comparePassword = async function (enteredPassword: string) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+
+schema.methods.generateToken = function () {
+return jwt.sign({_id: this._id}, process.env.JWT_SECRET || "", {
+    expiresIn: "2d"
+})
+};
+
+export const User = mongoose.model("user", schema);
