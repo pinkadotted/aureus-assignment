cd06415905c33b8241cb2fa3f6d76e1c6f40a2e2
pinkadotted <m.subesh@gmail.com>
2023-12-28 14:23:02 +0800

    added all the new routes

--- src/index.ts
+++ src/index.ts
@@ -25,22 +25,26 @@ app.get('/', (req: Request, res: Response) => {
 import { adminLogin, adminRegister, userLogin, userRegister } from "../controllers/authController";
 import { addJob, deleteJob } from "../controllers/admin";
 import { getAllJobs } from "../controllers/commonController";
-import { viewProfile } from "../controllers/user";
-import { isAuthenticated } from "../controllers/auth";
+import { applyForJob, viewAllApplications, viewJobDetails, viewProfile } from "../controllers/user";
+import { isAdmin, isAuthenticated, isUser } from "../controllers/auth";
 
 // routes for the user
 app.post("/user/signup", userRegister)
 app.post("/user/login", userLogin)
-app.get("/user/all-jobs", getAllJobs)
-app.get("/user/view-profile", isAuthenticated, viewProfile)
+app.get("/user/jobs",isAuthenticated, isUser, getAllJobs)
+app.get("/user/jobs/:id",isAuthenticated, isUser, viewJobDetails)
+app.post("/user/jobs/:id/apply",isAuthenticated, isUser, applyForJob)
+app.get("/user/applications",isAuthenticated, isUser, viewAllApplications)
+app.get("/user/me", isAuthenticated, isUser, viewProfile)
+
 
 
 // routes for the admin
 app.post("/admin/signup", adminRegister)
 app.post("/admin/login", adminLogin)
-app.post("/admin/add-job", addJob)
-app.get("/admin/all-jobs", getAllJobs)
-app.delete("/admin/delete-job/:id", deleteJob)
+app.post("/admin/add-job",isAuthenticated, isAdmin, addJob)
+app.get("/admin/all-jobs",isAuthenticated, isAdmin, getAllJobs)
+app.delete("/admin/delete-job/:id",isAuthenticated, isAdmin, deleteJob)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
