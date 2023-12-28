9a549532c8234d373c1f58e4854d4351c0458dee
pinkadotted <m.subesh@gmail.com>
2023-12-28 14:21:09 +0800

    changed user type to any for now

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,11 +1,10 @@
-// import { IUser } from '../../../models/user';
 import { User } from '../../models/user';
 import { Request } from 'express';
 
 declare global {
   namespace Express {
     export interface Request {
-      user: User;
+      user: any;
     }
   }
 }
