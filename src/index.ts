bb39521c0832ee8f4c50768102eac1f3dc05bf7b
pinkadotted <m.subesh@gmail.com>
2023-12-28 14:40:25 +0800

    rearraged the routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { connectDB } from "../db";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
+import { adminLogin, adminRegister, userLogin, userRegister } from "../controllers/authController";
+import { addJob, deleteJob } from "../controllers/admin";
+import { getAllJobs } from "../controllers/commonController";
+import { applyForJob, viewAllApplications, viewJobDetails, viewProfile } from "../controllers/user";
+import { isAdmin, isAuthenticated, isUser } from "../controllers/auth";
 
 dotenv.config();
 
@@ -22,29 +26,22 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!');
 });
 
-import { adminLogin, adminRegister, userLogin, userRegister } from "../controllers/authController";
-import { addJob, deleteJob } from "../controllers/admin";
-import { getAllJobs } from "../controllers/commonController";
-import { applyForJob, viewAllApplications, viewJobDetails, viewProfile } from "../controllers/user";
-import { isAdmin, isAuthenticated, isUser } from "../controllers/auth";
-
 // routes for the user
 app.post("/user/signup", userRegister)
 app.post("/user/login", userLogin)
-app.get("/user/jobs",isAuthenticated, isUser, getAllJobs)
-app.get("/user/jobs/:id",isAuthenticated, isUser, viewJobDetails)
 app.post("/user/jobs/:id/apply",isAuthenticated, isUser, applyForJob)
-app.get("/user/applications",isAuthenticated, isUser, viewAllApplications)
+app.get("/user/jobs/:id",isAuthenticated, isUser, viewJobDetails)
+app.get("/user/jobs",isAuthenticated, isUser, getAllJobs)
+app.get("/user/me/applications",isAuthenticated, isUser, viewAllApplications)
 app.get("/user/me", isAuthenticated, isUser, viewProfile)
-
-
 
 // routes for the admin
 app.post("/admin/signup", adminRegister)
 app.post("/admin/login", adminLogin)
-app.post("/admin/add-job",isAuthenticated, isAdmin, addJob)
-app.get("/admin/all-jobs",isAuthenticated, isAdmin, getAllJobs)
-app.delete("/admin/delete-job/:id",isAuthenticated, isAdmin, deleteJob)
+app.post("/admin/jobs/add",isAuthenticated, isAdmin, addJob)
+app.delete("/admin/jobs/delete/:id",isAuthenticated, isAdmin, deleteJob)
+app.get("/admin/jobs",isAuthenticated, isAdmin, getAllJobs)
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
