b854f9b7d60dc05dcb5ff01ff840c7d18a7fa2ed
pinkadotted <m.subesh@gmail.com>
2023-12-30 00:56:37 +0800

    wrapped fns in try catch

--- controllers/admin.ts
+++ controllers/admin.ts
@@ -6,28 +6,38 @@ import mongoose from 'mongoose';
 
 // adding a job 
 export const addJob = async (req: Request, res: Response) => {
-    connectDB();
-    // creating the job
-    const job = await Job.create(req.body);
-    // sending the response
-    res.status(201).json({ success: true, message: 'Job created successfully', job });
+    try {
+        connectDB();
+        // creating the job
+        const job = await Job.create(req.body);
+        // sending the response
+        res.status(201).json({ success: true, message: 'Job created successfully', job });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 }
 
 // deleting a job
 export const deleteJob = async (req: Request, res: Response) => {
-    const id  = req.params.id;
-
-    connectDB();
-
-    // checking if the job exists
-    const job = await Job.findById(id);
-    if (!job) {
-        res.status(404).json({ success: false, message: 'Job not found' });
+    try {
+        const id  = req.params.id;
+    
+        connectDB();
+    
+        // checking if the job exists
+        const job = await Job.findById(id);
+        if (!job) {
+            res.status(404).json({ success: false, message: 'Job not found' });
+        }
+        // deleting the job
+        const jobID = new mongoose.Types.ObjectId(id);
+        await Job.findByIdAndDelete(jobID);
+    
+        // sending the response
+        res.status(200).json({ success: true, message: 'Job deleted successfully', job });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    // deleting the job
-    const jobID = new mongoose.Types.ObjectId(id);
-    await Job.findByIdAndDelete(jobID);
-
-    // sending the response
-    res.status(200).json({ success: true, message: 'Job deleted successfully', job });
 }
